f54f716065cb9b51091319bd2b3b72d46d157797
CJustin23 <annt.b24dtcn181@stu.ptit.edu.vn>
2024-12-11 22:07:10 +0700

    Update s14b5.c

--- s14b5.c
+++ s14b5.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-	char word[100]="xin chao ten toi la Nguyen Manh Thang";
+	char word[100]="xin chao ten toi la Nguyen Truong An";
 	int length=strlen(word);
 	int count=0;
 	for(int i=0;i<length;i++){
